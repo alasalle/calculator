7938d28a1a5aa35f5eec7538b194e11a4ccaceea
alasalle <angela.lasalle91@gmail.com>
2020-11-09 19:02:31 -0500

    added long number handler to display

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,12 +1,29 @@
-import React, { useContext } from "react";
-import { NumberContext } from "./NumberProvider";
+import React, { useContext } from 'react';
+import { NumberContext } from './NumberProvider';
 
 const Display = () => {
   const { number, storedNum, functionType } = useContext(NumberContext);
+
+  const longNumHandler = (num) => {
+    console.log({ NUM: num, LENGTH: num.length });
+    let number = num.length > 10 ? parseFloat(num).toExponential(5) : num;
+    return number;
+  };
+
   return (
     <div>
-      <h1>{!number.length && !storedNum ? "0" : number || storedNum}</h1>
-      <p>{!storedNum ? "" : `${storedNum} ${functionType} ${number}`}</p>
+      <h1>
+        {!number.length && !storedNum
+          ? '0'
+          : longNumHandler(number) || longNumHandler(storedNum)}
+      </h1>
+      <p>
+        {!storedNum
+          ? ''
+          : `${longNumHandler(storedNum)} ${functionType} ${longNumHandler(
+              number
+            )}`}
+      </p>
     </div>
   );
 };
